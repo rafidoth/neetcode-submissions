86a4edc0f06fc802def66fe32a9c25df10922b70
S Rafiul Hasan <67283985+rafidoth@users.noreply.github.com>
2026-09-21 15:30:58 +0600

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public bool IsPalindrome(string s) {
+        string s1 = string.Concat(s.Where(c=> char.IsLetterOrDigit(c))).ToLower();
+        string s2 = string.Concat(s1.Reverse());
+        Console.WriteLine(s1);
+        Console.WriteLine(s2);
+        if(s1 == s2) return true;
+        return false;
+    }
+}
